ee1b8eadde9e926aa4b82779390eafaefddf4195
Clement-WANGWH <Clement.WANGWH@hotmail.com>
2023-11-18 19:46:00 +0800

    新增跳转功能

--- NEU_Restaurant/Pages/Detail.razor.cs
+++ NEU_Restaurant/Pages/Detail.razor.cs
@@ -1,6 +1,18 @@
-﻿namespace NEU_Restaurant.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using NEU_Restaurant.Library.IServices;
+using NEU_Restaurant.Library.Models;
+
+namespace NEU_Restaurant.Pages;
 
 public partial class Detail
 {
-	
+	[Inject]
+	public IDishStorage DishStorage { get; set; }
+
+	public Dish Dish { get; set; }
+
+	protected override async Task OnInitializedAsync()
+	{
+		Dish = await DishStorage.GetDishAsync(DishId);
+	}
 }
